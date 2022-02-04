b9ab91cc0797ece83bb341352f65575ffca7ebe7
AnneC <chretien.an@gmail.com>
2022-02-04 15:02:04 +0100

    building bases

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
 import React from "react";
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import What_Else from "./components/What_Else";
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
       <Skills />
-      <Testimonials />
+      <What_Else />
       <Contact />
     </main>
   );
